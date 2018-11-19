ea72911687baa378d92773b556d1714144935421
nicekkong <nicekkong@gmail.com>
2018-11-20 00:28:03 +0900

    second push

--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def year_archive(request, year):
     print("%s"%year)
-    return HttpResponse('{}년 입니다.'.format(year))
+    return HttpResponse('{}년 입니다....'.format(year))
